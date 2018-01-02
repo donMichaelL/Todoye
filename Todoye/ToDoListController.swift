55b59e02308de569d45412bc39c027ecea7599f3
Michael Loukeris <michael.loukeris@icloud.com>
2018-01-02 21:41:07 +0200

    The doto list without data persistence

---
+++ Todoye/ToDoListController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Todoye
+//
+//  Created by Michael Loukeris on 02/01/2018.
+//  Copyright © 2018 Michael Loukeris. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggs", "Do sports"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
